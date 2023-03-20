093936b1e2efd92b4f2d459ab533c3957fe901ca
Vineshvanga <121710301058@gitam.in>
2023-03-20 00:16:33 -0600

    Updated the heroku and nodemon setup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app=express();
 
 app.get('/', (req,res)=>{
-    res.send({'Hello there,': 'welcome to MCH'});
+    res.send({'Hello everyone,': 'welcome to MCH'});
 });
 
 
