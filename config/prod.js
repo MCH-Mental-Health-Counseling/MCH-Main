7436f4902ce36952582f89f4d4caaee86d2f4ff2
VineshVanga <121710301058@gitam.in>
2023-03-24 02:26:41 -0600

    Connecting server side and dialogflow

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
     googleProjectID: process.env.GOOGLE_PROJECT_ID,
     googlePrivateKeyID: process.env.GOOGLE_PRIVATE_KEY_ID,
-    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
+    googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
     dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
     dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUGAGE_CODE
