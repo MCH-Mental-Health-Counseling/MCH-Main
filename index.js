62b510e75240b9368c6dcc555fba28b2b113aa6d
VineshVanga <121710301058@gitam.in>
2023-03-24 01:10:18 -0600

    Training text and event dialogflow intents

--- index.js
+++ index.js
@@ -3,13 +3,9 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 const app=express();
 
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
-app.get('df/server', (req,res)=>{
-    res.send("hi from server")
-})
+app.use(bodyParser.json());
 
-require('./routes/dialogFlowRoutes')
+require('./routes/dialogFlowRoutes')(app);
 
 app.listen(port, ()=>{
     console.log("server is running");
--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,16 +1,28 @@
-const chatbot = require('../chatbot/chatbot')
-module.export = app =>{
-  app.get('text_query', async(req,res)=>{
+const chatbot = require('../chatbot/chatbot');
+
+module.exports = app =>{
+  app.get('/', (req,res)=>{
+    res.send({'hello':'jonny'})
+  });
+  app.post('/api/df_text_query', async(req,res)=>{
+    /*
     console.log(req)
-    const {text, userId} = req.body;
-    const resultQuery = await chatbot.textQuery(text, userId)
+    const {text,userId}=req.body;
+    const resultQuery = await chatbot.textQuery(text, userId);
     console.log(resultQuery)
-    res.send("Text Query")
-  })
-/*
-  app.post('/event_query', (req,res)=>{
-    console.log(req)
-    res.send("Text Query")
-  })
-  */
+    const resObj = {
+    fulfillmentText: resultQuery.fulfillmentText
+    };
+    res.send(resultQuery)
+    */
+    let responses = await chatbot.textQuery(req.body.text, req.body.parameters);
+    res.send(responses[0].queryResult);
+  });
+
+  app.post('/api/df_event_query',async(req,res)=>{
+    //let responses = await chatbot.eventQuery(req.body.event, req.body.parameters);
+    //res.send(responses[0].queryResults);
+    let responses = await chatbot.eventQuery(req.body.event, req.body.parameters);
+    res.send(responses[0].queryResult);
+  });
 }
