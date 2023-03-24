643512c32461219641ded53cad4f3fdf3ed305b0
VineshVanga <121710301058@gitam.in>
2023-03-24 01:44:37 -0600

    Connect server side and dialogflow

---
+++ chatbot/chatbot.js
@@ -0,0 +1,101 @@
+'use strict'
+
+const dialogflow = require('@google-cloud/dialogflow');
+const config = require('../config/keys');
+const {struct} = require('pb-util');
+
+const projectId = config.googleProjectID;
+const sessionId = config.dialogFlowSessionID;
+const languageCode = config.dialogFlowSessionLanguageCode;
+
+const credentials = {
+  client_email: config.googleClientEmail,
+  privateKey: config.googlePrivateKey
+}
+
+const sessionClient = new dialogflow.SessionsClient();
+const sessionPath = sessionClient.projectAgentSessionPath(
+  projectId,
+  sessionId
+);
+
+
+module.exports = {
+    textQuery: async function(text,parameters = {}){
+        let self = module.exports;
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                text: {
+                    text: text,
+                    languageCode: languageCode,
+                },
+            },
+            queryParams: {
+                payload:{
+                    data:parameters
+                }
+            }
+        };
+        let responses = await sessionClient.detectIntent(request);
+        responses = await self.handleAction(responses);
+        return responses;
+    },
+
+    eventQuery: async function(event,parameters = {}){
+        let self = module.exports;
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                event: {
+                    name: event,
+                    parameters: struct.encode(parameters),
+                    languageCode: languageCode,
+                },
+            },
+            queryParams: {
+                payload:{
+                    data:parameters
+                }
+            }
+        };
+        let responses = await sessionClient.detectIntent(request);
+        responses = await self.handleAction(responses);
+        return responses;
+    },
+
+
+    handleAction: function(responses){
+        return responses;
+    }
+}
+
+
+
+/*
+const textQuery = async(userText, userId)=>{
+    const request ={
+        session: sessionPath,
+        queryInput:{
+            text:{
+                text:userText,
+                languageCode:languageCode
+            }
+        }
+    }
+
+    try{
+        const response = await sessionClient.detectIntent(request)
+        console.log(response[0])
+        return response
+    }catch(err){
+        console.log(err)
+        return err
+    }
+}
+
+module.exports ={
+    textQuery,
+    eventQuery
+}
+*/
---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleProjectID: process.env.GOOGLE_PROJECT_ID,
+    googlePrivateKeyID: process.env.GOOGLE_PRIVATE_KEY_ID,
+    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
+    googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+    dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
+    dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUGAGE_CODE
+};
